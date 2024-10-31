8c17cf49cee95f5a1350b0d98975142db1b81c1b
unknown <heartchazer23553@gmail.com>
2024-10-31 17:57:49 +0500

    visual Script example

---
+++ lib/infinite_canvas.dart
@@ -0,0 +1,9 @@
+/// A Widget that renders a canvas that can be panned and zoomed.
+library infinite_canvas;
+
+export 'edge.dart';
+export 'graph.dart';
+export 'node.dart';
+export 'menu_entry.dart';
+export 'state/controller.dart';
+export 'canvas.dart';
